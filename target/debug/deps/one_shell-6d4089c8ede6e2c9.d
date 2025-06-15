ea7c2b64efdb4d3e4e0d879f97bd3f17298396bc
e-aub <eaub@proton.me>
2025-06-15 13:03:08 +0100

    new initial commit

---
+++ target/debug/deps/one_shell-6d4089c8ede6e2c9.d
@@ -0,0 +1,5 @@
+/home/aelhadda/0-shell/target/debug/deps/one_shell-6d4089c8ede6e2c9: src/main.rs
+
+/home/aelhadda/0-shell/target/debug/deps/one_shell-6d4089c8ede6e2c9.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/one-shell.d
@@ -0,0 +1,1 @@
+/home/aelhadda/0-shell/target/debug/one-shell: /home/aelhadda/0-shell/src/main.rs
